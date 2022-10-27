16e52974cf3188cb711248fed3c09d9aa52c8270
SimonBox <simon@resim.ai>
2022-10-27 15:58:20 -0700

    Expose port 8080 for local testing of mkdocs (#131)
    ## Description of change
    To build and test docs locally with `mkdocs serve` we need to open up a port
    in the docker container.
    ## Asana task link
    [Asana task](https://app.asana.com/0/0/1203255945592860/f)
    ## Guide to reproduce test results.
    ```
    mkdocs serve --dev-addr=0.0.0.0:8080 
    ```
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in this PR.

--- dev/run.sh
+++ dev/run.sh
@@ -1,1 +1,1 @@
-docker run -it --volume $(pwd):/workspaces/core --volume /var/run/docker.sock:/var/run/docker.sock 909785973729.dkr.ecr.us-east-1.amazonaws.com/core:latest bash
+docker run -it -p 8080:8080 --volume $(pwd):/workspaces/core --volume /var/run/docker.sock:/var/run/docker.sock 909785973729.dkr.ecr.us-east-1.amazonaws.com/core:latest bash
