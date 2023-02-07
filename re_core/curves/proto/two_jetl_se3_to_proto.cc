746db8e904f0a6f150434c03d7a4352327e40f91
iainjwhiteside <iain@resim.ai>
2023-02-07 03:52:27 -0800

    TwoJetL Packers/Unpackers (#231)
    ## Description of change
    
    Adds packers and unpackers for TwoJetL to and from protobuf. 
    
    It follows the pattern of introducing a protobuf message type for each possible flavour of TwoJetL, each with the same field names. 
    
    Packing and unpacking by and large calls the component packers, so most code in this PR is boilerplate.
    
    ## Asana task link
    
    [Asana task](https://app.asana.com/0/1203751014069901/1203869115126127/f)
    
    ## Guide to reproduce test results.
    
    ```
    bazel test //resim_core/curves/proto:two_jet_to_proto_test
    ```
    
    ## Checklist:
    - [X] I have self-reviewed this change.
    - [X] I have tested this change.
    - [X] This change is covered by tests that are already landed, or in this PR.

---
+++ re_core/curves/proto/two_jetl_se3_to_proto.cc
@@ -0,0 +1,22 @@
+#include "re_core/curves/proto/two_jetl_se3_to_proto.hh"
+
+#include <glog/logging.h>
+
+#include "re_core/curves/proto/two_jet.pb.h"
+#include "re_core/curves/proto/two_jet_to_proto.hh"
+#include "re_core/curves/two_jet.hh"
+#include "re_core/transforms/se3.hh"
+
+namespace resim::curves::proto {
+
+void pack(const TwoJetL<transforms::SE3> &in, TwoJetL_SE3 *const out) {
+  pack_two_jet(in, out);
+}
+
+TwoJetL<transforms::SE3> unpack(const TwoJetL_SE3 &in) {
+  TwoJetL<transforms::SE3> two_jet;
+  unpack_two_jet(in, InOut(two_jet));
+  return two_jet;
+}
+
+}  // namespace resim::curves::proto
---
+++ re_core/curves/proto/two_jetl_so3_to_proto.cc
@@ -0,0 +1,22 @@
+#include "re_core/curves/proto/two_jetl_so3_to_proto.hh"
+
+#include <glog/logging.h>
+
+#include "re_core/curves/proto/two_jet.pb.h"
+#include "re_core/curves/proto/two_jet_to_proto.hh"
+#include "re_core/curves/two_jet.hh"
+#include "re_core/transforms/so3.hh"
+
+namespace resim::curves::proto {
+
+void pack(const TwoJetL<transforms::SO3> &in, TwoJetL_SO3 *const out) {
+  pack_two_jet(in, out);
+}
+
+TwoJetL<transforms::SO3> unpack(const TwoJetL_SO3 &in) {
+  TwoJetL<transforms::SO3> two_jet;
+  unpack_two_jet(in, InOut(two_jet));
+  return two_jet;
+}
+
+}  // namespace resim::curves::proto
