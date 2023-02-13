17134254cd19fb4a2a915c7aaeecc9e310f86bb9
Sharon Lin <sharon@resim.ai>
2023-02-13 13:39:35 -0800

    DCurve packers and unpackers (#252)
    ## Description of change
    Adds packers and unpackers for DCurve.
    
    ## Asana task link
    [Asana task](https://app.asana.com/0/1203751014069901/1203869115126129/f)
    
    ## Guide to reproduce test results.
    `bazel test --test_output=all //resim_core/curves/proto:d_curve_to_proto_test`
    
    ## Checklist:
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [x] This change is covered by tests that are already landed, or in this PR.

---
+++ re_core/curves/proto/d_curve_se3_to_proto.cc
@@ -0,0 +1,22 @@
+#include "re_core/curves/proto/d_curve_se3_to_proto.hh"
+
+#include <glog/logging.h>
+
+#include "re_core/curves/proto/d_curve.pb.h"
+#include "re_core/curves/proto/d_curve_to_proto.hh"
+#include "re_core/transforms/se3.hh"
+#include "re_core/utils/inout.hh"
+
+namespace resim::curves::proto {
+
+void pack(const DCurve<transforms::SE3> &in, DCurve_SE3 *const out) {
+  pack_d_curve(in, out);
+}
+
+DCurve<transforms::SE3> unpack(const DCurve_SE3 &in) {
+  DCurve<transforms::SE3> d_curve_se3;
+  unpack_d_curve(in, InOut(d_curve_se3));
+  return d_curve_se3;
+}
+
+}  // namespace resim::curves::proto
