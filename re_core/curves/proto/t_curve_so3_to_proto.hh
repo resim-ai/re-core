2ffa024c9af3a905cf03a901abe8bb7c118ca31a
iainjwhiteside <iain@resim.ai>
2023-02-17 03:43:22 -0800

    TCurve Packers and Unpackers (#268)
    ## Description of change
    
    Adds packers and unpackers for TCurves to and from protobuf. 
    
    It follows the pattern of introducing a protobuf message type for each possible flavour of TCurve, each with the same field names and nested message. 
    
    Packing and unpacking by and large calls the component packers, so most code in this PR is boilerplate.
    
    ## Asana task link
    
    [Asana task](https://app.asana.com/0/1203751014069901/1203869115126127/f)
    
    ## Guide to reproduce test results.
    
    ```
    bazel test //resim_core/curves/proto:t_curve_to_proto_test
    ```
    
    ## Checklist:
    - [X] I have self-reviewed this change.
    - [X] I have tested this change.
    - [X] This change is covered by tests that are already landed, or in this PR.

---
+++ re_core/curves/proto/t_curve_so3_to_proto.hh
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "re_core/curves/proto/t_curve.pb.h"
+#include "re_core/curves/t_curve.hh"
+#include "re_core/transforms/so3.hh"
+
+namespace resim::curves::proto {
+
+void pack(const TCurve<transforms::SO3> &in, TCurve_SO3 *out);
+
+TCurve<transforms::SO3> unpack(const TCurve_SO3 &in);
+
+}  // namespace resim::curves::proto
