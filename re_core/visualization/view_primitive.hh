2456d2fc9894bd6aa78e2b9dc1005f1676f5a01b
iainjwhiteside <iain@resim.ai>
2023-04-06 18:29:34 +0100

    Frame to View Client (#317)
    ## Description of change
    
    As part of the custom naming of view objects, we extend view primitive to 
    enable the user to write resim::view << my frame
    
    ## Asana task link
    [Asana task](https://app.asana.com/0/1204081589980528/1204112383745307/f)
    ## Guide to reproduce test results.
    
    ```
    bazel test //resim_core/visualization/proto:view_primitive_to_proto_test
    bazel run //resim_core/visualization:view_test
    ```
    
    ## Checklist:
    - [X] I have self-reviewed this change.
    - [X] I have tested this change.
    - [X] This change is covered by tests that are already landed, or in this PR.

--- re_core/visualization/view_primitive.hh
+++ re_core/visualization/view_primitive.hh
@@ -7,6 +7,7 @@
 #include "re_core/actor/state/trajectory.hh"
 #include "re_core/curves/d_curve.hh"
 #include "re_core/curves/t_curve.hh"
+#include "re_core/transforms/frame.hh"
 #include "re_core/transforms/framed_group.hh"
 #include "re_core/transforms/se3.hh"
 #include "re_core/transforms/so3.hh"
@@ -26,7 +27,8 @@ struct ViewPrimitive {
       curves::DCurve<transforms::SE3>,
       curves::DCurve<transforms::FSE3>,
       curves::TCurve<transforms::FSE3>,
-      actor::state::Trajectory>
+      actor::state::Trajectory,
+      transforms::Frame<3>>
       payload;
 };
 
